cde68f43810b6b7671461cdd1b61814338317955
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2023-11-25 12:47:14 +0100

    Is this the real life or is this just fantasy
    Added queen functionality

--- Cpp_Chess/queen.cpp
+++ Cpp_Chess/queen.cpp
@@ -1,6 +1,142 @@
 #include "queen.h"
+#include "gameboard.h"
+
+bool Queen::UpAndDown(int X, int Y)
+{
+    bool ValidMove = true;
+    if((X-GetPosX()) == 0)
+    {
+        if(Y < GetPosY())
+        {
+            for (int I = GetPosY()-1 ; I >= Y; --I) {
+                if(GetGameField()->IsBlocked(X,I)||(GetGameField()->AbleToAttack(X,I) && !(I == Y)))
+                {
+                    ValidMove = false;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            for (int I = GetPosY()+1 ; I <= Y; ++I) {
+                if(GetGameField()->IsBlocked(X,I)||(GetGameField()->AbleToAttack(X,I) && !(I == Y)))
+                {
+                    ValidMove = false;
+                    break;
+                }
+            }
+        }
+        return ValidMove;
+    }
+    return false;
+}
+
+bool Queen::LeftAndRight(int X, int Y)
+{
+    bool ValidMove = true;
+    if((Y-GetPosY()) == 0)
+    {
+        if(X < GetPosX())
+        {
+            for (int I = GetPosX()-1 ; I >= X; --I) {
+                if(GetGameField()->IsBlocked(I,Y)||(GetGameField()->AbleToAttack(I,Y) && !(I == X)))
+                {
+                    ValidMove = false;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            for (int I = GetPosX()+1 ; I <= X; ++I) {
+                if(GetGameField()->IsBlocked(I,Y)||(GetGameField()->AbleToAttack(I,Y) && !(I == X)))
+                {
+                    ValidMove = false;
+                    break;
+                }
+            }
+        }
+        return ValidMove;
+    }
+    return false;
+}
+
+bool Queen::diagonal(int X, int Y)
+{
+    bool ValidMove = true;
+    if((abs(GetPosX()-X) == abs(GetPosY()-Y)) && ((GetPosX()-X) != 0))
+    {
+        if((GetPosY()-Y)<0) //Up
+        {
+            if((GetPosX()-X)<0)//Right
+            {
+                for (int I = 1 ; I <= abs(GetPosY()-Y); ++I) {
+                    int Xchecking = GetPosX() + I;
+                    int Ychecking = GetPosY() + I;
+                    std::cout<<"debug : "<< Xchecking <<" "<<Ychecking<<std::endl;
+                    if(GetGameField()->IsBlocked(Xchecking,Ychecking )||(GetGameField()->AbleToAttack(Xchecking,Ychecking ) && !(Ychecking  == Y)))
+                    {
+                        ValidMove = false;
+                        break;
+                    }
+                }
+            }
+            else if((GetPosX()-X)>0) //Left
+            {
+                for (int I = 1 ; I <= abs(GetPosY()-Y); ++I) {
+                    int Xchecking = GetPosX() - I;
+                    int Ychecking = GetPosY() + I;
+                    std::cout<<"debug : "<< Xchecking <<" "<<Ychecking<<std::endl;
+                    if(GetGameField()->IsBlocked(Xchecking,Ychecking )||(GetGameField()->AbleToAttack(Xchecking,Ychecking ) && !(Ychecking  == Y)))
+                    {
+                        ValidMove = false;
+                        break;
+                    }
+                }
+            }
+        }
+        else if((GetPosY()-Y)>0)//Down
+        {
+            if((GetPosX()-X)<0)//Right
+            {
+                for (int I = 1 ; I <= abs(GetPosY()-Y); ++I) {
+                    int Xchecking = GetPosX() + I;
+                    int Ychecking = GetPosY() - I;
+                    std::cout<<"debug : "<< Xchecking <<" "<<Ychecking<<std::endl;
+                    if(GetGameField()->IsBlocked(Xchecking,Ychecking )||(GetGameField()->AbleToAttack(Xchecking,Ychecking ) && !(Ychecking  == Y)))
+                    {
+                        ValidMove = false;
+                        break;
+                    }
+                }
+            }
+            else if((GetPosX()-X)>0)//Left
+            {
+                for (int I = 1 ; I <= abs(GetPosY()-Y); ++I) {
+                    int Xchecking = GetPosX() - I;
+                    int Ychecking = GetPosY() - I;
+                    std::cout<<"debug : "<< Xchecking <<" "<<Ychecking<<std::endl;
+                    if(GetGameField()->IsBlocked(Xchecking,Ychecking )||(GetGameField()->AbleToAttack(Xchecking,Ychecking ) && !(Ychecking  == Y)))
+                    {
+                        ValidMove = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return ValidMove;
+    }
+    return false;
+}
 
 bool Queen::Move(int X, int Y)
 {
+    if(LeftAndRight(X,Y)||UpAndDown(X,Y))
+    {
+        SetPosX(X);
+        SetPosY(Y);
+        return true;
+    }
     return false;
 }
+
--- Cpp_Chess/queen.h
+++ Cpp_Chess/queen.h
@@ -7,6 +7,9 @@ class Queen : public ChessPiece
 public:
     Queen(int X,int Y, bool Color, GameField*gamefield) : ChessPiece(X,Y,Color,gamefield)   {}
     bool Move(int X, int Y);
+private:
+    bool UpAndDown(int X, int Y);
+    bool LeftAndRight(int X, int Y);
 };
 
 #endif // QUEEN_H
